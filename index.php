0f25a614ae0cd171871fbb4d046e7278ad903191
kris <kris@xoofoo.org>
2013-06-09 15:54:36 +0200

    Focusing user name on login

--- index.php
+++ index.php
@@ -2236,6 +2236,7 @@ if(($this->getConfig('log_file') != null && strlen($this->getConfig('log_file'))
 <script type="text/javascript">
 //<![CDATA[
 $(document).ready(function() {
+	$('#user_name').focus();
 <?php
 	if(GateKeeper::isDeleteAllowed()){
 ?>
